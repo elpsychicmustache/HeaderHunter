96bc8ef176a2fe04811e191c3ceef084faa90d2f
elpsychicmustache <github@psychicmustache.com>
2025-12-17 20:31:41 -0700

    Prepared program for argparse.

--- src/headerhunter.py
+++ src/headerhunter.py
@@ -1,11 +1,41 @@
+import argparse
 import curses
 
+from models import connector
+
 def main(stdscr) -> None:
+    args:argparse.Namespace = get_argparse()
+    hostname:str = args.hostname
+
     stdscr.clear()
-    stdscr.addstr(0, 0, "Test 123", curses.A_BOLD)
-    stdscr.refresh()
+
+    if not hostname:
+        stdscr.addstr(0, 0, "-H/--hostname flag is currently required.")
+        stdscr.addstr(1, 0, "Press ENTER to quit.")
+        stdscr.refresh()
+    else:
+        stdscr.addstr(0, 0, f"TEST: {hostname}", curses.A_BOLD)
+        stdscr.refresh()
+
     stdscr.getch()
 
 
+def get_argparse() -> argparse.Namespace:
+    """Grabs the command-line arguments specified when the program was invoked.
+
+    :returns: The arguments given when starting the program.
+    :rtype: argparse.Namespace
+    """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-H", "--hostname",
+                        help="The host or target name.",
+                        default=None)
+
+    args = parser.parse_args()
+    return args
+
+
 if __name__ == "__main__":
+    args = get_argparse()
     curses.wrapper(main)
